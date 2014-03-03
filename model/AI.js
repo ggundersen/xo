d568ec79aa96410130f3a089cccaeb5e3ce2825f
Gregory Gundersen <ggundersen@gmail.com>
2014-03-03 13:09:35 -0500

    cleanup

--- model/AI.js
+++ model/AI.js
@@ -5,14 +5,11 @@ var AI = function(team) {
     this.team = team;
 };
 
-AI.prototype.move = function(board) {
-    //var move = this.suggestMove(board);
-    //console.log(move);
-    //board.add(move, this.team);
-
-};
-
 AI.prototype.suggestMove = function(board) {
+
+    //var randomX = Math.floor(Math.random() * board.N),
+    //    randomY = Math.floor(Math.random() * board.N);
+
     for (var i = 0; i < board.state.length; i++) {
         if (board.state[i] === 0) {
             // TODO: Use an access routine!
