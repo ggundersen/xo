d544d951edebfd61686c290fa17bfe9739c42e3f
Gregory Gundersen <ggundersen@gmail.com>
2014-03-05 09:01:42 -0500

    Simplified views; baked in some HTML because this is a web page

--- view/BoardView.js
+++ view/BoardView.js
@@ -3,14 +3,12 @@
 
 var BoardView = function(board, css) { 
 
-	var parentEl = document.getElementById('xo-container');
-	    el = document.createElement('div'),
-	    //squareDim = (css.board.width / board.N) - 2 * css.square.borderWidth,
+	var el = document.getElementById('board'),
 	    views = [];
 
 	el.id = 'board';
 	el.style.width = el.style.height = css.board.width + 'px';
-    parentEl.appendChild(el);
+    //parentEl.appendChild(el);
 
     _.each(board.state, function(val, index) {
         views.push(
